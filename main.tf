482c11e30706127135e3223a0d5726eb274b6970
ignacio-hernani <89387442+ignacio-hernani@users.noreply.github.com>
2024-07-03 15:32:36 +0200

    change availability zone

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "172.16.10.0/24"
-  availability_zone = "us-west-2a"
+  availability_zone = "eu-west-2a"
 
   tags = {
     Name = "tf-example"
