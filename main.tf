911b1bd5ddf503bb3d2e32dbad99de97fe5f69d0
ignacio-hernani <89387442+ignacio-hernani@users.noreply.github.com>
2024-07-18 11:56:55 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ resource "aws_network_interface" "foo" {
 
 resource "aws_instance" "foo" {
   ami           = "ami-0fe310dde2a8fdc5c" # us-west-2
-  instance_type = "t2.large"
+  instance_type = "t2.small"
   tags = {
     Name = var.instance_name
   }
